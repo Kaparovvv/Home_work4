cfe45983338ef1524888a6b394aa761ec161885f
Kaparovvv <kaparovkanagat@gmail.com>
2022-03-18 16:01:49 +0600

    First commit

---
+++ homework4.dart
@@ -0,0 +1,56 @@
+void main(List<String> args) {
+
+// 1
+  // // var num = 1;
+  // // for(var i = 1; i < 101; i++ ){
+  // //   print(i);
+  // // }
+
+  // var i = 1;
+  // while(i<=100){
+  //   print(i);
+  //   i++;
+  // }
+
+// 2
+  // int num1 = 11;
+  // while(num1<=33){
+  //   print(num1);
+  //   num1++;
+  // }
+  // int num2;
+  // for(int num2 = 11; num2<=33; num2++){
+  //   print(num2);
+  // }
+
+// 3 
+  // int num3 = 0;
+  // while(num3<=100){
+  //   print(num3.isEven);
+  //   num3++;
+  // }
+  // for(int num3=0; num3<=100; num3++){
+  // print(num3.isEven);
+  // }
+
+// 4
+  var num4 = 1;
+  var sum = 0;
+  while(num4<=100){
+    print(num4+=num4);
+    sum += num4;
+    num4++;
+  }
+
+  // for(int i=0; i<=100; i++){
+  //   num4 += i;
+  // }
+  // print(num4);
+
+
+// num sum = 0;
+// for (num e in [] ) {
+//   sum += e;
+//   print(sum);
+// }
+}
